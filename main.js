b04a803658b1a75932e56062d47dae9bd15f5f36
Star786-sus <77275680+Star786-sus@users.noreply.github.com>
2021-04-29 12:52:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+mouthX = 0
+mouthY = 0
+
+function preload()
+{
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("poseNet is intialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        mouthX = results[0].pose.nose.x-40;
+        mouthY = results[0].pose.nose.y;
+        console.log("mouthX =" + mouthX)
+        console.log("mouthY =" + mouthY)
+    }
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    image(mustache, mouthX, mouthY, 80, 35);
+}
+
+function takeSnapshot()
+{
+    save('mustache_filter.png');
+}
